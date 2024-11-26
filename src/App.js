4ebb3ffe3d21db716d1fe5610e33a9ff27335760
Stefano Casasola <ste.casasola@gmail.com>
2024-11-26 10:58:28 +0100

    Adds navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg'
 import './App.css'
 // allego il foglio di bootstrap, una volta per tutte
 // se ve lo dimenticate, non saranno disponibili le classi di bootstrap!
 import 'bootstrap/dist/css/bootstrap.min.css'
+import CustomNavbar from './components/CustomNavbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="mt-5 mb-5">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {/* passo due prop: una per il sottititolo della navbar e una che setterà il valore di fluid per il suo container */}
+        <CustomNavbar subtitle="Pastasciutte sul web" isFluid={true} />
       </header>
     </div>
   )
